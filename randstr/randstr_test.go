a8408a30df232765e3627f90b3ea564cbfe5d597
135yshr <isago@oreha.dev>
2023-06-21 20:52:43 +0900

    :sparkles: Chg parameterize test

--- randstr/randstr_test.go
+++ randstr/randstr_test.go
@@ -9,10 +9,36 @@ import (
 
 // TestGenerate tests the Dummy function.
 func TestGenerate(t *testing.T) {
-	g := randstr.NewDefaultGenerator()
-	require.NotNil(t, g)
+	type args struct {
+		length int
+	}
+	type want struct {
+		length  int
+		pattern string
+	}
+	tests := map[string]struct {
+		args args
+		want want
+	}{
+		"should return a string of length 5": {
+			args: args{
+				length: 5,
+			},
+			want: want{
+				length:  5,
+				pattern: "[a-z]+",
+			},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			g := randstr.NewDefaultGenerator()
+			require.NotNil(t, g)
 
-	str := g.Generate(5)
-	require.Len(t, str, 5)
-	require.Regexp(t, "[a-z]+", str)
+			str := g.Generate(tt.args.length)
+			require.Len(t, str, tt.want.length)
+			require.Regexp(t, tt.want.pattern, str)
+		})
+	}
+
 }
