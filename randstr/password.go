1a805739a9e5f792a472f3e2c9f51be177c4b289
135yshr <isago@oreha.dev>
2023-06-21 22:47:32 +0900

    :rotating_light: Del unnecessary code

--- randstr/password.go
+++ randstr/password.go
@@ -1,7 +1,5 @@
 package randstr
 
-type passwordGenerator stringGenerator
-
 // NewPasswordGenerator creates a new password generator
 func NewPasswordGenerator() Generator {
 	return newStringGenerator(lowercase + uppercase + numbers + symbols)
